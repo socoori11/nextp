0f6359b7f8ab8b496bdefad1ba71f0e7c31fe9ba
socoori11 <socoori@naver.com>
2025-06-18 16:08:03 +0900

    11번번째 수정

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,12 +1,14 @@
-// pages/api/logout.ts
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   // 쿠키에서 'token'을 삭제하여 로그아웃 처리
-  res.setHeader('Set-Cookie', 'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT')
+  res.setHeader('Set-Cookie', 
+    'token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=None; Max-Age=0'
+  )
   
   // 로그아웃 성공 메시지 반환
   res.status(200).json({ message: '로그아웃 완료' })
 }
 
 export default handler
+
